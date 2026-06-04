423191aa7cb44c510b7d7c938280dcdb770a326a
ollyfryguy <ollyfryguy@gmail.com>
2026-06-03 17:39:34 -0700

    Basic Frame Concatenation

--- src/camera/cameraHandler.py
+++ src/camera/cameraHandler.py
@@ -1,20 +1,28 @@
 import cv2
+#from cv2_enumerate_cameras import enumerate_cameras
 import numpy
 import threading
-
-defaultWidth = 1920
-defaultHeight = 1080
 
 arducamL = cv2.VideoCapture(0)
 arducamR = cv2.VideoCapture(1)
 
-class VideoStream:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+if not arducamL.isOpened() or not arducamR.isOpened():
+    print("ERROR: Could not open camera")
+
+while True:
+    recievedL, lFrame = arducamL.read()
+    recievedR, rFrame = arducamR.read()
+    
+    if not recievedL or not recievedR:
+        print("Didn't recieve frame. Exiting...")
+        break
+    
+    hStackFrame = cv2.hconcat([lFrame, rFrame])
+    
+    cv2.imshow('Horizontally Stacked Frame', hStackFrame)
+    if cv2.waitKey(0):
+        break
 
 
-        if height == None or width == None:
-                self.height = defaultHeight
-                self.width = defaultWidth
-
+arducamL.release()
+cv2.destroyAllWindows()
